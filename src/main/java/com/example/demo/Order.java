953525abf06932311f8f8eb4737ca17ba381a5f8
JJongMani <3827925@naver.com>
2020-04-28 11:10:06 +0900

    commit

---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ORDER_TABLE")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    Long orderId;
+    Long productId;
+    int qty;
+    String productName;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
